3ca8d21a71e2ace3d57c6e57511d1793ca5d7981
ripee <ripee.dev@gmail.com>
2017-12-06 16:56:50 -0500

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -23,7 +23,7 @@
 # *not* include it on all devices, so it is safe even with hardware-specific
 # components.
 
-ifneq ($(filter gts210ltexx,$(TARGET_DEVICE)),)
+ifneq ($(filter gts28ltexx,$(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 
