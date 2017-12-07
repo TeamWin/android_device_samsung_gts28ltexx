2abe727bebd5d8485371325386ca6bd4ffb8a60b
ripee <ripee.dev@gmail.com>
2017-12-07 11:32:45 -0500

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -65,8 +65,3 @@ RECOVERY_TOUCHSCREEN_FLIP_Y := true
 
 # Encryption support
 TW_INCLUDE_CRYPTO := true
-
-# Init properties from bootloader version, ex. model info
-TARGET_UNIFIED_DEVICE := true
-TARGET_INIT_VENDOR_LIB := libinit_gts28ltexx
-TARGET_LIBINIT_DEFINES_FILE := $(DEVICE_TREE)/init/init_gts28ltexx.cpp
